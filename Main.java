dceb89c23e7b685dbbdc5cbabfb13226d3a4d155
John-Rendell <112700505+John-Rendell@users.noreply.github.com>
2022-09-19 19:03:53 +0800

    Java Swing

---
+++ Main.java
@@ -0,0 +1,8 @@
+/**
+ * Creator: John Rendell Catuiza, Sana sagutin na ako ng crush ko huhu :( :(
+ * **/
+public class Main {
+    public static void main(String[] args) {
+        new myMotherfckingframe();
+    }
+}
